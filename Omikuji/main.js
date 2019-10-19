94da3e7b01df84aa7420e74a81d5824129d30e90
UXHD-Yuyu <rsrconvoy@gmail.com>
2019-10-19 17:13:03 +0900

    omikuji update

--- Omikuji/main.js
+++ Omikuji/main.js
@@ -1,18 +1,21 @@
-window.omload = function() {
-    const imgsrc = new Array('daikiti.png','tyukiti.png','kiti.png','kyou.png','daikyou.png');
+window.onload = function() {
+    const imgSrc = new Array('img/daikiti.png','img/tyukiti.png','img/kiti.png','img/kyou.png','img/daikyou.png');
+    const resetSrc = 'img/omikuji.png';
 
     const startBtn = document.getElementById('start-Btn');
     const resetBtn = document.getElementById('reset-btn');
     const omikujiImg = document.getElementById('omikuji-img');
 
     startBtn.onclick = function() {
-        const random = math.floor(math.random() * imgsrc.length);
-        omikujiImg.src[random];
+        const random = Math.floor(Math.random() * imgSrc.length);
+        omikujiImg.src = imgSrc[random];
 
-        if (random == 0) {
+        if (random == 0){
             alert('良い一日になりそうだ!');
         } else if (random == 4) {
             alert('嫌な予感がする');
+        } else {
+            alert('noploblem');
         }
     }
 
